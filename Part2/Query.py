242241472409b98b808f32cfc72ff0aa575324d8
ralck <ralck@users.noreply.github.com>
2018-03-25 23:17:28 -0400

    Initial commit of part 2

---
+++ Part2/Query.py
@@ -0,0 +1,65 @@
+import sqlite3
+import dateutil.parser
+import datetime
+import sys
+
+def ValidateArgs():
+    if len(sys.argv) is not 3:
+        print("Usage: Python query.py start_date end_date")
+
+    startDate = ''
+    endDate = ''
+
+    try:
+        startDate = dateutil.parser.parse(sys.argv[1])
+        endDate = dateutil.parser.parse(sys.argv[2])
+    except:
+        print("One ore more invalid date strings.")
+        return (False, startDate, endDate)
+
+    return (True, startDate, endDate)
+
+
+def RunQuery(sDate, eDate):
+    print("count,week,workflow_state")
+
+    # Validate sDate starts a week and eDate ends a week
+    if not sDate.weekday() == 0:
+        sDate += datetime.timedelta(days=(7-sDate.weekday()))
+    if not eDate.weekday() == 0:
+        eDate -= datetime.timedelta(days=eDate.weekday())
+
+    currentDate = sDate
+
+    # one query per week
+    conn = sqlite3.connect('applicants.sqlite3')
+    curs = conn.cursor()
+
+    while(currentDate < eDate):
+        dates = (currentDate, currentDate + datetime.timedelta(days=7))
+
+        curs.execute('''SELECT COUNT(created_at), workflow_state FROM applicants WHERE created_at >= ? AND created_at < ? GROUP BY workflow_state''', dates)
+        sqlResults = curs.fetchall()
+        for result in sqlResults:
+            print('{0},{1},{2}'.format(result[0], currentDate.date(), result[1]))
+        # print(sqlResults)
+
+        currentDate = currentDate + datetime.timedelta(days=7)
+
+    # One query, post processing in Python
+    # dates = (sDate,eDate)
+    #
+    # conn = sqlite3.connect('applicants.sqlite3')
+    # curs = conn.cursor()
+    # curs.execute('''SELECT workflow_state, created_at FROM applicants WHERE created_at >= ? AND created_at <= ?''', dates)
+    # sqlResults = curs.fetchall()
+
+    #print(sqlResults)
+
+    conn.close()
+
+
+if(__name__=='__main__'):
+    result = ValidateArgs()
+    if(result[0]):
+        RunQuery(result[1], result[2])
